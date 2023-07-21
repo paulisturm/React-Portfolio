ed143a71b5b0bad3ea450ccc99fe706e08f44919
paulisturm <paulisturm22@gmail.com>
2023-07-20 21:47:42 -0400

    icons

--- Portfolio App/src/components/Footer.js
+++ Portfolio App/src/components/Footer.js
@@ -1,21 +1,31 @@
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { fab, faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+  
+
+  library.add(fab, faLinkedin, faGithub, faInstagram)
+
 
 function Footer () {
     return (
       <footer>
         <div>
-            <ul className="list-group list-group-horizontal justify-content-center">
+            <ul className="list-group list-group-horizontal justify-content-center fixed-bottom">
                 <a href="https://github.com/paulisturm">
-                <li class="list-group-item">github link</li>
+                <li class="list-group-item"><FontAwesomeIcon icon={faGithub} size="4x" /></li>
                 </a>
                 <a href="www.linkedin.com/in/paul-sturm-199a6a218">
-                <li class="list-group-item">linkedIn link</li>
+                <li class="list-group-item"><FontAwesomeIcon icon={faLinkedin} size="4x" /></li>
                 </a>
                 <a href="https://www.instagram.com/paul.sturm/">
-                <li class="list-group-item">Instagram link</li>
+                <li class="list-group-item"><FontAwesomeIcon icon={faInstagram} size="4x" /></li>
                 </a>
             </ul>
+            <div>
+    
+  </div>
         </div>
       </footer>
     
--- Portfolio App/src/components/Portfolio.js
+++ Portfolio App/src/components/Portfolio.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Portfolio () {
     return (
-        <><h1>Portfolio</h1><p className='portfolio'> list of my projects</p></>
+        <><h1>Portfolio</h1>
+        <p className='portfolio'> Check Out Some Of My Recent Projects!</p>
+        <div className='projects'>
+            <div href="https://github.com/paulisturm/PWA-Text-Editor">PWA Text Editor</div>
+            <div href="https://github.com/paulisturm/Social-Network-API">Project 2</div>
+            <div href="https://github.com/paulisturm/SVG-Logo-Maker">Project 3</div>
+            <div href="https://github.com/paulisturm/SQL-Employee-Tracker">Project 4</div>
+            <div href="https://github.com/paulisturm/ORM-E-Commerce-Backend">Project 5</div>
+            <div href="https://github.com/paulisturm/Javascript-Password-Generator">Project 6</div>
+        </div>
+        </>
         
     );
 }
