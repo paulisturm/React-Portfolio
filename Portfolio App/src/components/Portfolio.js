be3e254c38b10b3c89299f077c85f2754a0c8116
paulisturm <paulisturm22@gmail.com>
2023-07-31 18:34:13 -0400

    portfolio

--- Portfolio App/src/components/Portfolio.js
+++ Portfolio App/src/components/Portfolio.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Jate from '../assets/JATE.png';
 
 function Portfolio () {
     return (
         <><h1>Portfolio</h1>
         <p className='portfolio'> Check Out Some Of My Recent Projects!</p>
         <div className='projects'>
-            <div href="https://github.com/paulisturm/PWA-Text-Editor">PWA Text Editor</div>
-            <div href="https://github.com/paulisturm/Social-Network-API">Project 2</div>
-            <div href="https://github.com/paulisturm/SVG-Logo-Maker">Project 3</div>
-            <div href="https://github.com/paulisturm/SQL-Employee-Tracker">Project 4</div>
-            <div href="https://github.com/paulisturm/ORM-E-Commerce-Backend">Project 5</div>
-            <div href="https://github.com/paulisturm/Javascript-Password-Generator">Project 6</div>
+            <div href="https://github.com/paulisturm/PWA-Text-Editor" img src={Jate}>PWA Text Editor</div>
+            <div href="https://github.com/paulisturm/Social-Network-API">Social Network API</div>
+            <div href="https://github.com/paulisturm/SVG-Logo-Maker">SVG Logo Maker</div>
+            <div href="https://github.com/paulisturm/SQL-Employee-Tracker">SQL Employee Tracker</div>
+            <div href="https://github.com/paulisturm/ORM-E-Commerce-Backend">ORM E Commerce Backend</div>
+            <div href="https://github.com/paulisturm/Javascript-Password-Generator">Javascript Password Generator</div>
         </div>
         </>
         
